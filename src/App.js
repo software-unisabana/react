1956d1109c4db1823690fed590ec575c37d1c4be
Samuel Espitia <samuelespitia88@gmail.com>
2023-04-05 22:12:17 -0500

    Agregada la función eliminar

--- src/App.js
+++ src/App.js
@@ -2,30 +2,78 @@ import { useState } from "react"
 
 export const CourseList = ({cursos}) => {
 
+    const Actualizar = () => {
+
+        setLista(
+            <>
+                <table>
+                    <tbody >
+                        <tr>
+                            <th> ID </th>
+                            <th> NOMBRE </th>
+                            <th> DESCRIPCIÓN </th>
+                            <th> ELIMINAR </th>
+                        </tr>
+    
+                        {cursos.map(curso => 
+                        {
+                            return (<CourseItem item = {curso} Eliminar = {Eliminar}/>)
+                        })}
+    
+                    </tbody>
+                    </table> <br/><br/>
+                
+                <h1>Nuevo Curso</h1>
+                
+                <table>
+                    <tbody >
+    
+                        <tr>
+                            <td> Id: </td>
+                            <td> <input id="GetId" type="number" name="id" /> </td>
+                        </tr>
+    
+                        <tr>
+                            <td> Nombre: </td>
+                            <td> <input id="GetNombre" type="text" name="nombre"/> </td>
+                        </tr>
+    
+                        <tr>
+                            <td> Descripcion: </td>
+                            <td> <input id="GetDescripcion" type="text" name="descripcion"/> </td>
+                            
+                        </tr>
+    
+                    </tbody>
+                </table> <br/><br/>
+    
+                <button onClick={Guardar}>Guardar</button>
+            </>
+            )
+
+    }
+
     const Eliminar = (event, id) => {
 
         cursos = cursos.filter(curso => curso.id != id);
 
-        setLista(
-        <>
-            <table>
-                <tbody >
-                    <tr>
-                        <th> ID </th>
-                        <th> NOMBRE </th>
-                        <th> DESCRIPCIÓN </th>
-                        <th> ELIMINAR </th>
-                    </tr>
+        Actualizar()
+    }
 
-                    {cursos.map(curso => 
-                    {
-                        return (<CourseItem item = {curso}/>)
-                    })}
+    const Guardar = () => {
+        const id = document.getElementById('GetId').value;
+        const nombre = document.getElementById('GetNombre').value;
+        const descripcion = document.getElementById('GetDescripcion').value;
 
-                </tbody>
-            </table>
-        </>
-        )
+        if ((id) && (nombre) && (descripcion)) {
+
+            cursos.push({id: id, nombre: nombre, descripcion: descripcion})
+            Actualizar()
+
+        }else{
+            alert('Ingrese todos los datos')    
+        }
+
     }
 
     const CourseItem = ({item}) => {
@@ -58,13 +106,40 @@ export const CourseList = ({cursos}) => {
                     })}
 
                 </tbody>
-            </table>
+            </table> <br/><br/>
+            
+            <h1>Nuevo Curso</h1>
+            
+            <table>
+                <tbody >
+
+                    <tr>
+                        <td> Id: </td>
+                        <td> <input id="GetId" type="number" name="id" /> </td>
+                    </tr>
+
+                    <tr>
+                        <td> Nombre: </td>
+                        <td> <input id="GetNombre" type="text" name="nombre"/> </td>
+                    </tr>
+
+                    <tr>
+                        <td> Descripcion: </td>
+                        <td> <input id="GetDescripcion" type="text" name="descripcion"/> </td>
+                        
+                    </tr>
+
+                </tbody>
+            </table> <br/><br/>
+
+            <button onClick={Guardar}>Guardar</button>
         </>
     )
 
     return (lista)
 
 }
+
 
 CourseList.defaultProps = {
     cursos: [
