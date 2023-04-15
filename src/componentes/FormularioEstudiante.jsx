669f3cedecdc738cad28dab882581bc357b5a5c5
joseph8884 <josegzm08@gmail.com>
2023-04-15 12:33:51 -0500

    Funcion eliminar terminada

--- src/componentes/FormularioEstudiante.jsx
+++ src/componentes/FormularioEstudiante.jsx
@@ -29,7 +29,7 @@ export const FormularioEstudiante = ({ agregar }) => {
             <form onSubmit={guardarEstudiante}>
                 <div className="form-group ">
                     <label htmlFor="id">ID Estudiante</label>
-                    <input type="id" className="form-control" id="id" placeholder="Ingrese id" value={id} onChange={(event) => setId(event.target.value)} />
+                    <input type="number" className="form-control" id="id" placeholder="Ingrese id" value={id} onChange={(event) => setId(event.target.value)} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="nombre">Nombre</label>
@@ -37,7 +37,7 @@ export const FormularioEstudiante = ({ agregar }) => {
                 </div>
                 <div className="form-group">
                     <label htmlFor="semestre">Semestre</label>
-                    <input type="text" className="form-control" id="semestre" placeholder="semestre" value={semestre} onChange={(event) => setSemestre(event.target.value)} />
+                    <input type="number" className="form-control" id="semestre" placeholder="semestre" value={semestre} onChange={(event) => setSemestre(event.target.value)} />
                 </div>
 
                 <button type="submit" className="btn btn-primary">Registrar</button>
