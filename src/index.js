ae710445d652139219ed3ccb5a90d1326105d4c6
Santiago Santafe <andressasi@unisabana.edu.co>
2023-04-17 13:34:10 -0500

    Adding css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { EstudiantesApp } from './EstudianteApp';
 import 'bootstrap/dist/css/bootstrap.css';
+import './estilos.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
