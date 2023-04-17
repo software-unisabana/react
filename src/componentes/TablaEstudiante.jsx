d4e9c5dae8c71b2974f77fe78761767b70223264
Abril <juanabca@unisabana.edu.co>
2023-04-17 13:41:17 -0500

    filtro

--- src/componentes/TablaEstudiante.jsx
+++ src/componentes/TablaEstudiante.jsx
@@ -1,46 +1,71 @@
 import { useState } from "react";
 
 export const TablaEstudiante = ({ listaEstudiantes }) => {
-  const [buscar, setBuscar] = useState("");
-  const [estudianteEncontrado, setEstudianteEncontrado] = useState(null);
-
+  // const [buscarName, setBuscarName] = useState("");
+  const [buscarFac, setBuscarFac] = useState("");
+  // const [estudianteEncontradoName, setEstudianteEncontradoName] = useState(null);
+  const [estudianteEncontradoFac, setEstudianteEncontradoFac] = useState(null);
+ 
 
   const editar = () => {
         
   }
+
   function deleteRow(event) {
       const row = event.target.parentNode.parentNode;
       row.parentNode.removeChild(row);
   }
 
-  const buscarEstudiantePorNombre = (nombre) => {
-    return listaEstudiantes.find((estudiante) => estudiante.nombre === nombre);
+  const buscarEstudiantePorFacultad = (facultad) => {
+    return listaEstudiantes.find((estudiante) => estudiante.facultad == facultad)
+  }
+
+  // const buscarEstudiantePorNombre = (nombre) => {
+  //   return listaEstudiantes.find((estudiante) => estudiante.nombre === nombre);
+  // };
+
+  const buscarEstFac = (event) => {
+    event.preventDefault();
+    const elementoEncontradoFac = buscarEstudiantePorFacultad(buscarFac);
+    setEstudianteEncontradoFac(elementoEncontradoFac);
   };
 
-  const buscarEst = (event) => {    
-    event.preventDefault();
-    const elementoEncontrado = buscarEstudiantePorNombre(buscar);
-    setEstudianteEncontrado(elementoEncontrado);
-  };
+  // const buscarEstName = (event) => {    
+  //   event.preventDefault();
+  //   const elementoEncontradoName = buscarEstudiantePorNombre(buscarName);
+  //   setEstudianteEncontradoName(elementoEncontradoName);
+  // };
 
   const limpiarBusqueda = () => {
-    setBuscar("");
-    setEstudianteEncontrado(null);
+    // setBuscarName("");
+    setBuscarFac("");
+    // setEstudianteEncontradoName(null);
+    setEstudianteEncontradoFac(null);
   };
 
   return (
     <>
-      <form onSubmit={buscarEst}>
+      <form onSubmit={buscarEstFac}>
         <div className="form-group">
-          <label htmlFor="buscar">Buscar</label>
-          <input type="text" className="form-control" id="buscar" placeholder="Buscar estudiante" value={buscar} onChange={(event) => setBuscar(event.target.value)} />
+          <label htmlFor="buscarFac">Buscar Facultad</label>
+          {/* <input type="text" className="form-control" id="buscarName" placeholder="Buscar " value={buscarFac} onChange={(event) => setBuscarFac(event.target.value)} /> */}
+          <br></br>
+          <select ClassName="form-control" id="facultad" placeholder="facultad" value={buscarFac} onChange={(event) => setBuscarFac(event.target.value)}>
+            <option value="">--Seleccione la facultad--</option>
+            <option value="Ingenieria">Ingenieria</option>
+            <option value="Medicina">Medicina</option>
+            <option value="Comunicacion">Comunicacion</option>
+            <option value="Educacion">Educacion</option>
+            <option value="Derecho">Derecho</option>
+          </select>
+          <br></br>
           <button type="submit" className="btn btn-primary"> Buscar
           </button>
         </div>
       </form>
 
       {
-      estudianteEncontrado ? (
+      estudianteEncontradoFac ? (
         <div>
           <h3>Estudiante encontrado:</h3>
           <table className="table">
@@ -49,14 +74,19 @@ export const TablaEstudiante = ({ listaEstudiantes }) => {
                 <th scope="col">Id Estudiante</th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Semestre</th>
+                <th scope="col">Facultad</th>
               </tr>
             </thead>
             <tbody>
-              <tr key={estudianteEncontrado.id}>
-                <td>{estudianteEncontrado.id}</td>
-                <td>{estudianteEncontrado.nombre}</td>
-                <td>{estudianteEncontrado.semestre}</td>
-              </tr>
+            {listaEstudiantes.map((estudiante) => (
+                <tr key={estudiante.id}>
+                  <td>{estudiante.id}</td>
+                  <td>{estudiante.nombre}</td>
+                  <td>{estudiante.semestre}</td>
+                  <td>{estudiante.facultad}</td>
+                  
+                </tr>
+              ))}
             </tbody>
           </table>
           <button className="btn btn-info" onClick={limpiarBusqueda}> Limpiar búsqueda
