308e259ee92c599554abcc397454d522b5506a68
juanMS <285juanda@gmail.com>
2023-04-17 13:14:04 -0500

    Se finaliza validaciones para las variables id y name

--- src/componentes/FormularioEstudiante.jsx
+++ src/componentes/FormularioEstudiante.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export const FormularioEstudiante = ({ agregar , buscarName }) => {
-    const [id, setId] = useState("");
+    const [id, setId] = useState(0);
     const [nombre, setNombre] = useState("");
     const [semestre, setSemestre] = useState("");
     const [facultad , setFacultad] = useState("")
@@ -10,17 +10,22 @@ export const FormularioEstudiante = ({ agregar , buscarName }) => {
     const guardarEstudiante = (event) => {
         event.preventDefault();
 
-        let estudiante = {
-            id: id,
-            nombre: nombre,
-            semestre: semestre,
-            facultad: facultad
+        if(6 <= id <= 10 && nombre.length > 3){
+            let estudiante = {
+                id: id,
+                nombre: nombre,
+                semestre: semestre,
+                facultad: facultad
+            }
+            agregar(estudiante)
+            setId("");
+            setNombre("");
+            setSemestre("");
+            setFacultad("");
+        }else{
+            alert("El id debe tener entre 6-10 caracteres y el nombre mas de 3 caracteres")
         }
-        agregar(estudiante)
-        setId("");
-        setNombre("");
-        setSemestre("");
-        setFacultad("");
+        
     }
 
 
@@ -29,7 +34,7 @@ export const FormularioEstudiante = ({ agregar , buscarName }) => {
             <form onSubmit={guardarEstudiante}>
                 <div className="form-group ">
                     <label htmlFor="id">ID Estudiante</label>
-                    <input type="id" className="form-control" id="id" placeholder="Ingrese id" value={id} onChange={(event) => setId(event.target.value)} />
+                    <input type="number" className="form-control" id="id" placeholder="Ingrese id" value={id} onChange={(event) => setId(event.target.value)} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="nombre">Nombre</label>
