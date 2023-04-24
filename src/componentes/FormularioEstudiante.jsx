dde05e54694ba02caa2b427d36bf276f5569fa65
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-04-23 20:51:04 -0500

    Add new presentation style

--- src/componentes/FormularioEstudiante.jsx
+++ src/componentes/FormularioEstudiante.jsx
@@ -14,21 +14,36 @@ export const FormularioEstudiante = ({ agregar, dato, setDato }) => {
     return (
         <>
             <form onSubmit={guardarEstudiante}>
-                <div className="form-group ">
-                    <label htmlFor="id">ID Estudiante</label>
-                    <input type="id" className="form-control" id="id" placeholder="Ingrese id" value={dato.id} onChange={(event) => setDato({ ...dato, id: event.target.value })} />
+                <div className="row">
+                    <div class="col-2 ">
+                        <label htmlFor="id">ID Estudiante</label>
+                    </div>
+                    <div class="col-2 ">
+                        <input type="id" className="form-control" id="id" placeholder="Id" value={dato.id} onChange={(event) => setDato({ ...dato, id: event.target.value })} />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="nombre">Nombre</label>
-                    <input type="text" className="form-control" id="nombre" placeholder="nombre" value={dato.nombre} onChange={(event) => setDato({ ...dato, nombre: event.target.value })} />
+                <div className="row">
+                    <div class="col-2 ">
+                        <label htmlFor="nombre">Nombre</label>
+                    </div>
+                    <div class="col-2 ">
+                        <input type="text" className="form-control" id="nombre" placeholder="Nombre Estudiante" value={dato.nombre} onChange={(event) => setDato({ ...dato, nombre: event.target.value })} />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="semestre">Semestre</label>
-                    <input type="text" className="form-control" id="semestre" placeholder="semestre" value={dato.semestre} onChange={(event) => setDato({ ...dato, semestre: event.target.value })} />
+                <div className="row">
+                    <div class="col-2 ">
+                        <label htmlFor="semestre">Semestre</label>
+                    </div>
+                    <div class="col-2 ">
+                        <input type="text" className="form-control" id="semestre" placeholder="Semestre" value={dato.semestre} onChange={(event) => setDato({ ...dato, semestre: event.target.value })} />
+                    </div>
                 </div>
-
-                <button type="submit" className="btn btn-primary">Registrar</button>
-            </form>
+                <div className="row">
+                    <div className="col-md-4 text-center" >
+                        <button type="submit" className="btn btn-primary">Registrar</button>
+                    </div>
+                </div>
+            </form >
         </>
     )
 }
