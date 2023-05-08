99731b9953490d899c21aaa1b939d19ba64452fc
BryamDigar <bryamdiaz1010@gmail.com>
2023-05-08 13:50:38 -0500

    Se elimina el input de id

--- src/componentes/FormularioEstudiante.jsx
+++ src/componentes/FormularioEstudiante.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 
 export const FormularioEstudiante = ({ agregar , buscarName }) => {
-    const [id, setId] = useState(0);
     const [nombre, setNombre] = useState("");
     const [semestre, setSemestre] = useState("");
     const [facultad , setFacultad] = useState("")
@@ -11,14 +10,12 @@ export const FormularioEstudiante = ({ agregar , buscarName }) => {
         event.preventDefault();
 
         let estudiante = {
-            id: id,
             nombre: nombre,
             semestre: semestre,
             facultad: facultad
         }
 
         agregar(estudiante)
-        setId("");
         setNombre("");
         setSemestre("");
         setFacultad("");
@@ -28,10 +25,6 @@ export const FormularioEstudiante = ({ agregar , buscarName }) => {
     return (
         <>
             <form onSubmit={guardarEstudiante}>
-                <div className="form-group ">
-                    <label htmlFor="id">ID Estudiante</label>
-                    <input type="number" className="form-control" id="id" placeholder="Ingrese id" value={id} onChange={(event) => setId(event.target.value)} />
-                </div>
                 <div className="form-group">
                     <label htmlFor="nombre">Nombre</label>
                     <input type="text" className="form-control" id="nombre" placeholder="nombre" value={nombre} onChange={(event) => setNombre(event.target.value)} />
